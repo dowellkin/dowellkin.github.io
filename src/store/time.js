bce9b3791a0c628f00cc435b06c8c56a8343fdec
dowellkin <dowellkin@gmail.com>
2020-07-19 18:22:40 +0300

    current day fix

--- src/store/time.js
+++ src/store/time.js
@@ -20,18 +20,23 @@ export default {
 
 	getters: {
 		getHours(state){
-			return state.date.getHours().toString()
+			return state.date.getHours()
 		},
 		getMinutes(state){
 			return state.date.getMinutes().toString().length < 2 
-							? "0" + state.date.getMinutes().toString()
-							: state.date.getMinutes().toString()
+							? "0" + state.date.getMinutes()
+							: state.date.getMinutes()
 		},
 		getTime(state, getters){
 			return `${getters.getHours}:${getters.getMinutes}`
 		},
-		getDay(state){
-			return state.date.getDay() - 0;
+		getDay(state) {
+			state
+			return 0
+			// return state.date.getDay() == 0 ? 6 : state.date.getDay() - 1;
+		},
+		getEncodedTime(state, getters) {
+			return (parseInt(getters.getHours, 10) * 60) + parseInt(getters.getMinutes, 10)
 		}
 	}
 }
