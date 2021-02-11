7ee3fd9ac632406d1c60fe0cfd3c065dc9c882fe
Alexandr Korolev <dowellkin@gmail.com>
2021-02-11 19:08:23 +0300

    add search to teachers

--- src/views/teachers.vue
+++ src/views/teachers.vue
@@ -59,7 +59,59 @@
 				<a-button v-else type="primary" size="small" icon="tool"  @click='showModal("Сhange", record.id)'>
 					{{$t('Look')}}
 				</a-button>
-			</span>
+				</span>
+				<div
+				slot="filterDropdown"
+				slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }"
+				style="padding: 8px"
+			>
+				<a-input
+					v-ant-ref="c => (searchInput = c)"
+					:placeholder="`Search ${column.dataIndex}`"
+					:value="selectedKeys[0]"
+					style="width: 188px; margin-bottom: 8px; display: block;"
+					@change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
+					@pressEnter="() => handleSearch(selectedKeys, confirm, column.dataIndex)"
+				/>
+				<a-button
+					type="primary"
+					icon="search"
+					size="small"
+					style="width: 90px; margin-right: 8px"
+					@click="() => handleSearch(selectedKeys, confirm, column.dataIndex)"
+				>
+					Search
+				</a-button>
+				<a-button size="small" style="width: 90px" @click="() => handleReset(clearFilters)">
+					Reset
+				</a-button>
+			</div>
+			<a-icon
+				slot="filterIcon"
+				slot-scope="filtered"
+				type="search"
+				:style="{ color: filtered ? '#108ee9' : undefined }"
+			/>
+			<template slot="customRender" slot-scope="text, record, index, column">
+				<span v-if="searchText && searchedColumn === column.dataIndex">
+					<template
+						v-for="(fragment, i) in text
+							.toString()
+							.split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))"
+					>
+						<mark
+							v-if="fragment.toLowerCase() === searchText.toLowerCase()"
+							:key="i"
+							class="highlight"
+							>{{ fragment }}</mark
+						>
+						<template v-else>{{ fragment }}</template>
+					</template>
+				</span>
+				<template v-else>
+					{{ text }}
+				</template>
+			</template>
 		</a-table>
 	</div>
 </template>
@@ -83,6 +135,23 @@ export default {
 					title: this.$t('Name'),
 					dataIndex: 'name',
 					key: 'name',
+          scopedSlots: {
+            filterDropdown: 'filterDropdown',
+            filterIcon: 'filterIcon',
+            customRender: 'customRender',
+          },
+          onFilter: (value, record) =>
+            record.name
+              .toString()
+              .toLowerCase()
+              .includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+              setTimeout(() => {
+                this.searchInput.focus();
+              }, 0);
+            }
+          },
 				},
 				{
 					title: this.$t('Link'),
@@ -107,7 +176,10 @@ export default {
 				formVariable: this.$form.createForm(this, { name: 'coordinated' }),
 				nameStatus: "",
 				help: "",
-			}
+			},
+      searchText: '',
+      searchInput: null,
+      searchedColumn: '',
 		}
 	},
 	computed: {
@@ -178,7 +250,17 @@ export default {
 			} else {
 				callback(rule.message);
 			}
-		}
+		},
+    handleSearch(selectedKeys, confirm, dataIndex) {
+      confirm();
+      this.searchText = selectedKeys[0];
+      this.searchedColumn = dataIndex;
+    },
+
+    handleReset(clearFilters) {
+      clearFilters();
+      this.searchText = '';
+    },
 	}
 }
 </script>
