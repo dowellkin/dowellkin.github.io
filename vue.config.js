15e36d666f30317e410135fe2c0ccb0ebb2567a2
Alexandr Korolev <dowellkin@gmail.com>
2021-10-01 09:40:38 +0300

    new logo

--- vue.config.js
+++ vue.config.js
@@ -24,105 +24,159 @@ module.exports = {
 			name: "SCHEDO",
 			themeColor: '#1890ff',
 			msTileColor: '#1890ff',
+  		background_color: "#001529",
 			manifestOptions: {
 				"icons": [
 					{
-						"src": "img/icons/android-launchericon-512-512.png",
+						"src": "img/icons/maskable/maskable_icon_x48.png",
+						"type": "image/png",
+						"sizes": "48x48",
+						"purpose": "maskable"
+					},
+					{
+						"src": "img/icons/maskable/maskable_icon_x72.png",
+						"type": "image/png",
+						"sizes": "72x72",
+						"purpose": "maskable"
+					},
+					{
+						"src": "img/icons/maskable/maskable_icon_x96.png",
+						"type": "image/png",
+						"sizes": "96x96",
+						"purpose": "maskable"
+					},
+					{
+						"src": "img/icons/maskable/maskable_icon_x128.png",
+						"type": "image/png",
+						"sizes": "128x128",
+						"purpose": "maskable"
+					},
+					{
+						"src": "img/icons/maskable/maskable_icon_x192.png",
+						"type": "image/png",
+						"sizes": "192x192",
+						"purpose": "maskable"
+					},
+					{
+						"src": "img/icons/maskable/maskable_icon_x384.png",
+						"type": "image/png",
+						"sizes": "384x384",
+						"purpose": "maskable"
+					},
+					{
+						"src": "img/icons/maskable/maskable_icon_x512.png",
+						"type": "image/png",
+						"sizes": "512x512",
+						"purpose": "maskable"
+					},
+
+					{
+						"src": "img/icons/icon-512x512.png",
 						"type": "image/png",
 						"sizes": "512x512",
 					},
 					{
-						"src": "img/icons/android-launchericon-192-192.png",
+						"src": "img/icons/icon-192x192.png",
 						"type": "image/png",
 						"sizes": "192x192",
-						"purpose": "any maskable"
 					},
 					{
-						"src": "img/icons/android-launchericon-144-144.png",
-						"type": "image/png",
+						"src": "img/icons/apple-icon-144x144.jpg",
+						"type": "image/jpg",
 						"sizes": "144x144",
 					},
 					{
-						"src": "img/icons/android-launchericon-96-96.png",
-						"type": "image/png",
+						"src": "img/icons/favicon-96x96.jpg",
+						"type": "image/jpg",
 						"sizes": "96x96",
 					},
 					{
-						"src": "img/icons/android-launchericon-72-72.png",
-						"type": "image/png",
+						"src": "img/icons/apple-icon-72x72.jpg",
+						"type": "image/jpg",
 						"sizes": "72x72",
 					},
+					// {
+					// 	"src": "img/icons/android-launchericon-48-48.png",
+					// 	"type": "image/png",
+					// 	"sizes": "48x48",
+					// },
 					{
-						"src": "img/icons/android-launchericon-48-48.png",
-						"type": "image/png",
-						"sizes": "48x48",
-					},
-					{
-						"src": "img/icons/apple-icon-180x180.png",
-						"type": "image/png",
+						"src": "img/icons/apple-icon-180x180.jpg",
+						"type": "image/jpg",
 						"sizes": "180x180",
 					},
 					{
-						"src": "img/icons/apple-icon-152x152.png",
-						"type": "image/png",
+						"src": "img/icons/apple-icon-152x152.jpg",
+						"type": "image/jpg",
 						"sizes": "152x152",
 					},
 					{
-						"src": "img/icons/apple-icon-144x144.png",
-						"type": "image/png",
+						"src": "img/icons/apple-icon-144x144.jpg",
+						"type": "image/jpg",
 						"sizes": "144x144"
 					},
 					{
-						"src": "img/icons/apple-icon-120x120.png",
-						"type": "image/png",
+						"src": "img/icons/apple-icon-120x120.jpg",
+						"type": "image/jpg",
 						"sizes": "120x120"
 					},
 					{
-						"src": "img/icons/apple-icon-114x114.png",
-						"type": "image/png",
+						"src": "img/icons/apple-icon-114x114.jpg",
+						"type": "image/jpg",
 						"sizes": "114x114"
 					},
+					// {
+					// 	"src": "img/icons/favicon-96x96.jpg",
+					// 	"type": "image/jpg",
+					// 	"sizes": "96x96"
+					// },
 					{
-						"src": "img/icons/favicon-96x96.png",
-						"type": "image/png",
-						"sizes": "96x96"
-					},
-					{
-						"src": "img/icons/apple-icon-76x76.png",
-						"type": "image/png",
+						"src": "img/icons/apple-icon-76x76.jpg",
+						"type": "image/jpg",
 						"sizes": "76x76"
 					},
 					{
-						"src": "img/icons/apple-icon-72x72.png",
-						"type": "image/png",
+						"src": "img/icons/apple-icon-72x72.jpg",
+						"type": "image/jpg",
 						"sizes": "72x72"
 					},
 					{
-						"src": "img/icons/apple-icon-60x60.png",
-						"type": "image/png",
+						"src": "img/icons/apple-icon-60x60.jpg",
+						"type": "image/jpg",
 						"sizes": "60x60"
 					},
 					{
-						"src": "img/icons/apple-icon-57x57.png",
-						"type": "image/png",
+						"src": "img/icons/apple-icon-57x57.jpg",
+						"type": "image/jpg",
 						"sizes": "57x57"
 					},
 					{
-						"src": "img/icons/favicon-96x96.png",
-						"type": "image/png",
+						"src": "img/icons/favicon-96x96.jpg",
+						"type": "image/jpg",
 						"sizes": "96x96"
 					},
 					{
-						"src": "img/icons/favicon-32x32.png",
-						"type": "image/png",
+						"src": "img/icons/favicon-32x32.jpg",
+						"type": "image/jpg",
 						"sizes": "32x32"
 					},
 					{
-						"src": "img/icons/favicon-16x16.png",
-						"type": "image/png",
+						"src": "img/icons/favicon-16x16.jpg",
+						"type": "image/jpg",
 						"sizes": "16x16"
-					}
+					},
 				],
-			}
+
+				shortcuts: [
+					{
+						name: "Schedule",
+						url: "/"
+					},
+					{
+						name: "links",
+						url: "/links/"
+					}
+				]
+			},
 		}
 }
